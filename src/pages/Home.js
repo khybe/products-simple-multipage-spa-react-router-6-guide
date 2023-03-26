b0445d5459a68d41dfa84b095a48552e03fb80c4
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-26 22:48:09 +0200

    Navigating between Pages with Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <h2>Home Page!</h2>;
+  return (
+    <>
+      <h2>Home Page!</h2>
+      <p>
+        Go to <Link to="/products">the products page</Link>
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
